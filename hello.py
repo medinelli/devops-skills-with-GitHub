331bf54afb8eaf65f4dd4dfab70405dd92710dc5
lmedinelli <luis.medinelli@gmail.com>
2022-11-12 21:55:05 +0000

    makefile and first file hello.py & tests

---
+++ hello.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# build an trigger function
+def add(a,b):
+    return a+b
+
+#build a substract function
+def substract(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
+if __name__ == "__main__":
+    print("Using multiply 5 and 6")
+    x = multiply(5,6)
+    print(x)
---
+++ test_hello.py
@@ -0,0 +1,16 @@
+from hello import add, substract, multiply, divide
+
+def test_add():
+    assert add(3,5) == 8
+
+#build a test for substract
+def test_substract():
+    assert substract(10,5)==5
+
+def test_multiply():
+    assert multiply(5,5) == 25
+    assert multiply(4,4) == 16
+
+def test_divide():
+    assert divide(10,2) == 5
+    assert divide(5,2) == 2
